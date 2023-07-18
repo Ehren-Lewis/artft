bba188a07bda54a7daa1e098989ff0b114a4b802
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2023-07-17 22:21:30 -0500

    added the changes from main to the push to github

--- app/page.js
+++ app/page.js
@@ -1,13 +1,29 @@
-import Image from 'next/image'
+
 import PopularSell from '@/components/popularSellers'
-import MoreSellers from '@/components/moreSellers'
+import MoreSellers from '@/components/moreSellers';
+import Wallet from '@/components/wallet';
+// import ethereumVar from '@/metamask/main';
+
+
+
+
 export default function Home() {
+
+
+  // ethereumVar.request({ method: 'eth_requestAccounts', params: [] });
+
+  // console.log(ethereumVar);
+
+
   return (
+
+
+
     <main className="container mx-auto">
         <div>
           <PopularSell />
           <MoreSellers />
-
+          <Wallet />
         </div>
     </main>
   )
