a447e4c219e2c5751ecb99abce304e86c8c2af0a
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2023-07-29 22:40:15 -0500

    added the guide for the user home page, tested image pull from IPFS

--- components/navbar.js
+++ components/navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <nav className="columns-3 flex justify-around items-center p-10 bg-black shadow-lg nav-wrapper">
             <Link href="/" className="">ArtFT</Link>
-            <Link href="/account"><FontAwesomeIcon icon={faUser} className="h-6 w-6 nav-icon"/></Link>
+            <Link href="/"><FontAwesomeIcon icon={faUser} className="h-6 w-6 nav-icon"/></Link>
             <Link href="/artists/login" className="nav-anchor">Artist Login</Link>
             <SearchBar id="searchBox"  formclass="w-1/2" inputclass="w-full p-3 bg-slate-900 rounded-full" />
                 {/* <FontAwesomeIcon icon={faMagnifyingGlass} className="h-6 w-6" style={{color: "#fff"}} /> */}
